52fef327090c15ebcdeb1c7e45581ee7ff0edd35
Iasmin Santos <iasminwinx@gmail.com>
2022-03-16 18:00:51 -0300

    Enviando o projeto

---
+++ src/components/Home.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="div">
+        <h1>Refresh the page and get advice ❤</h1>
+        <div class="box">
+            <ul>
+                <p
+                    v-for="f of Frase"
+                    :key="f.id">
+                    {{ f.advice }}
+                </p>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyHome',
+        data(){
+            return{
+                Frase:[]
+            }
+        },
+        created(){
+            fetch('https://api.adviceslip.com/advice')
+            .then(response => response.json())
+            .then(json => {this.Frase = json;})
+        }
+    }
+
+</script>
+
+<style scoped>
+.div{
+    margin-top:10%;
+    height: 25vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.box{
+    width: 50%;
+    height: 100%;
+    background-color: rgb(179, 19, 112);
+    text-align: center;
+    font-family:Comic Sans MS, Comic Sans, cursive;
+    border-radius:5px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+p{
+    width: 100%;
+    color:rgb(255, 255, 255);
+    font-size: 20px;
+}
+</style>
---
+++ src/components/shared/TopBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+    <v-app-bar app color="pink" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        Advice for a Click
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="green--text text--lighten-2">
+    
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              Início
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              Entrar
+            </v-list-item-title>
+          </v-list-item>
+          </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      drawer: false,
+      group: null,
+    }
+  }
+}
+</script>
